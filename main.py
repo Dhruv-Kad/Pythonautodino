79978ad454a60d89cf46e514646e662812bb2792
jrich <jrichardson99652@gmail.com>
2025-09-15 12:32:50 -0500

    Somewhat ok prototype

--- main.py
+++ main.py
@@ -2,67 +2,69 @@ import pyautogui
 import time
 import getpixelcolor
 import keyboard
-import os
 import webbrowser
 
 class Jump:
     def short_press():
-        pyautogui.keyDown("space")
-        time.sleep(0.001)
-        pyautogui.keyUp("space")
+        pyautogui.press("space")
+        print("JUMP!!")
     def long_press():
         pyautogui.keyDown("space")
-        time.sleep(0.01)
+        time.sleep(0.001)
         pyautogui.keyUp("space")
 
 class Crouch:
     def short_crouch():
         pyautogui.keyDown("down")
-        time.sleep(0.1)
+        time.sleep(0.0001)
         pyautogui.keyUp("down")
     def long_crouch():
         pyautogui.keyDown("down")
-        time.sleep(0.75)
+        time.sleep(0.001)
         pyautogui.keyUp("down")
-
-def clear_terminal():
-    # For Windows
-    if os.name == 'nt':
-        os.system('cls')
-    # For macOS and Linux
-    else:
-        os.system('clear')
 
 #To open the dino game
 
-#webbrowser.open("https://chromedino.com/#google_vignette")
+webbrowser.open("https://chromedino.com/#google_vignette")
 
 
 BLACK = 83
 WHITE = 247
 
-time.sleep(5)
+time.sleep(3)
 pyautogui.press('space')
 
+x = 320
+y = 425
+x2 = 265
+
+dino_x = 148
+dino_y = 428
+
+
+jumps = 0 #for testing (to see where it jumps)
 while True:
-    x = 248
-    y = 420
-    r, g, b = getpixelcolor.pixel(x, y)
+    r, g, b = getpixelcolor.average(x2, y, 55, 1)
+    #r2, g2, b2 = getpixelcolor.pixel(x2, y)
+    dino_r, dino_g, dino_b = getpixelcolor.pixel(dino_x, dino_y)
+    game_over_r, game_over_g, game_over_b = getpixelcolor.pixel(423, 334)
+
     if r != WHITE:
-        Jump.short_press()
+        if dino_r != WHITE:
+            Jump.short_press()
+            jumps += 1
+        else:
+            time.sleep(0.008)
+            Jump.short_press()
+            jumps += 1
     else:
         pass
 
-    if keyboard.is_pressed('s'):
+    if game_over_r == BLACK:
+        print(jumps)
         break
 
-    #This is to find the mouse position
-    '''
-    clear_terminal()
-    x, y = pyautogui.position()
-    print(x, y)
-    if keyboard.is_pressed('s'):
-        break '''
+#Record: 453, avg: ~250
 
 
 
