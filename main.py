d848841a5230222433fd3a9c774c1d766aa335dd
jrich <jrichardson99652@gmail.com>
2025-09-15 19:05:34 -0500

    Code used in meeting

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pyautogui
 import time
 import getpixelcolor
-import keyboard
 import webbrowser
 
 class Jump:
-    def short_press():
-        pyautogui.press("space")
-        print("JUMP!!")
+    def short_press(): 
+        pyautogui.keyDown("space")
+        pyautogui.keyUp("space") 
     def long_press():
         pyautogui.keyDown("space")
         time.sleep(0.001)
@@ -16,7 +15,7 @@ class Jump:
 class Crouch:
     def short_crouch():
         pyautogui.keyDown("down")
-        time.sleep(0.0001)
+        time.sleep(0.1)
         pyautogui.keyUp("down")
     def long_crouch():
         pyautogui.keyDown("down")
@@ -34,17 +33,18 @@ WHITE = 247
 time.sleep(3)
 pyautogui.press('space')
 
-x = 320
+x = 310
 y = 425
-x2 = 265
+x2 = 290
+y2 = 385
 
 dino_x = 148
 dino_y = 428
 
 
-jumps = 0 #for testing (to see where it jumps)
 while True:
     r, g, b = getpixelcolor.average(x2, y, 55, 1)
+    r2, g2, b2, = getpixelcolor.average(x2, y2, 55, 1) 
     #r2, g2, b2 = getpixelcolor.pixel(x2, y)
     dino_r, dino_g, dino_b = getpixelcolor.pixel(dino_x, dino_y)
     game_over_r, game_over_g, game_over_b = getpixelcolor.pixel(423, 334)
@@ -52,19 +52,16 @@ while True:
     if r != WHITE:
         if dino_r != WHITE:
             Jump.short_press()
-            jumps += 1
         else:
-            time.sleep(0.008)
-            Jump.short_press()
-            jumps += 1
-    else:
-        pass
+            time.sleep(0.1)
+            Jump.short_press()  
+    elif r2 != WHITE:
+        Crouch.short_crouch()
 
     if game_over_r == BLACK:
-        print(jumps)
         break
 
-#Record: 453, avg: ~250
+#Record: 611, Meeting Best: 201
 
 
 
